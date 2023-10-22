88b3ecbc761c3751572781d4bb8af75c0076d2f1
Demizo <demizodemazo@gmail.com>
2023-10-21 19:55:36 -0500

    Update status.c

--- config/boards/shields/nice_view/widgets/status.c
+++ config/boards/shields/nice_view/widgets/status.c
@@ -147,7 +147,7 @@ static void draw_middle(lv_obj_t *widget, lv_color_t cbuf[], const struct status
     };
 
     for (int i = 0; i < 5; i++) {
-        bool selected = i == state->active_profile_index;
+        bool selected = i == state->layer_index;
 
         lv_canvas_draw_arc(canvas, circle_offsets[i][0], circle_offsets[i][1], 13, 0, 359,
                            &arc_dsc);
@@ -174,7 +174,7 @@ static void draw_bottom(lv_obj_t *widget, lv_color_t cbuf[], const struct status
     init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
     lv_draw_label_dsc_t label_dsc;
     init_label_dsc(&label_dsc, LVGL_FOREGROUND, &lv_font_montserrat_14, LV_TEXT_ALIGN_CENTER);
-
+    
     // Fill background
     lv_canvas_draw_rect(canvas, 0, 0, CANVAS_SIZE, CANVAS_SIZE, &rect_black_dsc);
 
@@ -234,8 +234,8 @@ static void set_output_status(struct zmk_widget_status *widget,
     widget->state.active_profile_bonded = state->active_profile_bonded;
     
     draw_top(widget->obj, widget->cbuf, &widget->state);
-    draw_middle(widget->obj, widget->cbuf2, &widget->state);
-    draw_bottom(widget->obj, widget->cbuf2, &widget->state);
+    //draw_middle(widget->obj, widget->cbuf2, &widget->state);
+    //draw_bottom(widget->obj, widget->cbuf2, &widget->state);
 }
 
 static void output_status_update_cb(struct output_status_state state) {
@@ -267,7 +267,7 @@ static void set_layer_status(struct zmk_widget_status *widget, struct layer_stat
     widget->state.layer_index = state.index;
     widget->state.layer_label = state.label;
 
-    //draw_middle(widget->obj, widget->cbuf3, &widget->state);
+    draw_middle(widget->obj, widget->cbuf3, &widget->state);
 }
 
 static void layer_status_update_cb(struct layer_status_state state) {
